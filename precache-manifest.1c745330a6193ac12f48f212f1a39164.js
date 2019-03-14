c6073d6fb767c67ff64a417c4f84c1f48c43a7ee
mwomack <michael.womack84@gmail.com>
2019-03-14 14:29:21 -0700

    Updates

---
+++ precache-manifest.1c745330a6193ac12f48f212f1a39164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b3e93421dd7ff864081",
+    "url": "/Contact-Manager/static/js/runtime~main.f93e6c74.js"
+  },
+  {
+    "revision": "4159d60d4f0cefcf5fe2",
+    "url": "/Contact-Manager/static/js/main.cd1a96e3.chunk.js"
+  },
+  {
+    "revision": "817d37a0886ea407f53b",
+    "url": "/Contact-Manager/static/js/2.23bbb1a7.chunk.js"
+  },
+  {
+    "revision": "4159d60d4f0cefcf5fe2",
+    "url": "/Contact-Manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "817d37a0886ea407f53b",
+    "url": "/Contact-Manager/static/css/2.2a91bda1.chunk.css"
+  },
+  {
+    "revision": "b0bd9dfa5d6c21380de548c4f258502b",
+    "url": "/Contact-Manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d0da19acd5f70fc61034f2b462900d1.js"
+  "/Contact-Manager/precache-manifest.1c745330a6193ac12f48f212f1a39164.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Contact-Manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
